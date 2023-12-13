4b1ff717e7ec6b1cf2635a665e86481db8110316
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2023-12-13 18:49:10 +0900

    Time: 77 ms (32.75%), Space: 51.3 MB (78.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-   let buy = prices[0] 
-   let maxProfit = 0;
-   for(let i=0; i<prices.length; i++) {
-     if(prices[i]-buy > maxProfit) {
-         maxProfit = prices[i]-buy
-     }
-     if(prices[i] < buy) {
-         buy = prices[i]
-     }
-   }
-   return maxProfit
+    let maxProfit = 0;
+    let minPrice = prices[0];
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+
+    return maxProfit;
 };
