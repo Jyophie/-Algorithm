3943518497e58529229673f5110a2bf9f818996c
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-01-11 12:06:16 +0900

    Time: 48 ms (91.54%), Space: 43.2 MB (18.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,21 +2,39 @@
  * @param {string} s
  * @return {boolean}
  */
+
+const pairs = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+}
+     
 var isValid = function(s) {
-    let arr = [];
     
-    for(let str of s){
-        if (str === '(' || str === '[' || str === '{') {
-            arr.push(str);
-        } else {
-            if (arr.length === 0 || 
-                (str ===')' && arr[arr.length-1] !== '(') || 
-                (str ===']' && arr[arr.length-1] !== '[') || 
-                (str ==='}' && arr[arr.length-1] !== '{')) {
-                return false;
-            }
-            arr.pop();
+    // check if length is an odd number. if it is, return false
+    // if there is an odd number, it means that at least one bracket is missing a pair
+    if (s.length % 2 === 1) return false
+    
+    // if the first element is a closing bracket, it doesn't matter what follows
+    // it will never have a pair
+    if (s[0] === "]" || s[0] === ")" || s[0] === "}") return false
+    
+    // same goes for last element, we are just dealing with opening bracket
+    if (s[s.length - 1] === "[" || s[s.length - 1] === "(" || s[s.length - 1] === "{") return false
+    
+    
+    let stack = []
+    
+    for(let i=0; i<s.length; i++) {
+        // if it's an opening bracket, push into stack
+        // else, assume it's closing bracket and check if it matches anything
+        if(s[i] === "[" || s[i] === "(" || s[i] === "{") {
+            stack.push(s[i])
+        } else if (pairs[stack.pop()] !== s[i]) {
+            return false
         }
+        
     }
-    return !arr.length;
+    return stack.length === 0
+    
 };
