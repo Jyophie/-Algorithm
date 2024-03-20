502ce43524564b08dae5bb997dd80fbcf375ff80
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-03-20 18:49:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 3.65 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    var answer = 0;
+    let n = 1;
+    let flag = false;
+
+    while(!flag){
+        n++;
+        
+        for(let i = 1; i < arr.length; i++){
+            if((arr[0] * n) % arr[i] === 0){
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+    }
+    
+    answer = arr[0] * n;
+    return answer;
+}
