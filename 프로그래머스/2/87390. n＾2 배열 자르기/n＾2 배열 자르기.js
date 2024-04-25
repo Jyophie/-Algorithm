0372cf0bb303328254614e474d8b9fa6eab3a126
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-04-25 12:49:05 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.72 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,16 +1,39 @@
-function solution(n, left, right) {
-    var answer = [];
-    let y = Math.floor(left / n);
-    let x = left % n;
+// function solution(n, left, right) {
+//     let answer = [];
     
-    for(let i = 0; i <= right-left; i++){
-        answer.push(Math.max(x,y)+1);
-        if(x+1 < n){
-            x++;
-        } else {
-            y++;
-            x = 0;
-        }
+//     for(let i = 1; i <= n; i++){
+//         for(let j = 1; j <= n; j++){
+//             if(i < j){
+//                 answer.push(j);
+//             } else {
+//                 answer.push(i);
+//             }
+//         }
+//     }
+    
+//     return answer.slice(left, right+1);
+// }
+
+// 2차원 배열을 만들지 않더라도 구하고자 하는 위치의 값을 보면
+// 1. (x, y) 의 최댓값에서 +1을 해주면 해당 좌표의 값이 나온다.
+
+// 그럼 좌표를 어떻게 구할까?
+// 2. (Math.floor(left / n), left % n)
+
+function solution(n, left, right) {
+    let answer = [];
+
+    while(left <= right) {
+
+        // 2)
+        let value = [Math.floor(left / n), left % n];
+
+        // 1)
+        value = Math.max(value[0], value[1]);
+        answer.push(value + 1);
+
+        left += 1;
     }
+
     return answer;
 }
