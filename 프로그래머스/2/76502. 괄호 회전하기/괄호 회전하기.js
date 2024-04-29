0245fcdf3c281ab2487e022586523840187b6f47
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-04-29 12:56:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,19 +1,21 @@
 function solution(s) {
-  let answer = 0;
+    let answer = 0;
+    for (let i = 0; i < s.length; i++) {
+        const arr = [];
+        const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
+        // 문자열 s를 i 칸만큼 왼쪽으로 회전시킨 결과를 temp 변수에 저장 - 회전 문자열 만들기
     
-  for (let i = 0; i < s.length; i++) {
-    const arr = [];
-    const temp = i === 0 ? s : s.slice(i) + s.slice(0, i);
-    
-    for (let j = 0; j < temp.length; j++) {
-      if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
-      else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
-      else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
-      else arr.push(temp[j]);
+        // for문 돌면서 괄호가 문제 없이 잘 닫히는지 확인
+        for (let j = 0; j < temp.length; j++) {
+            if (arr[arr.length - 1] === '(' && temp[j] === ')') arr.pop();
+            else if (arr[arr.length - 1] === '[' && temp[j] === ']') arr.pop();
+            else if (arr[arr.length - 1] === '{' && temp[j] === '}') arr.pop();
+            else arr.push(temp[j]);
+        }
+        
+        // 배열이 비어있으면 모두 괄호가 완성되므로 카운트 +1 하기
+        if (arr.length === 0) answer++;
     }
-    
-      if (arr.length === 0) answer++;
-  }
     
   return answer;
 }
