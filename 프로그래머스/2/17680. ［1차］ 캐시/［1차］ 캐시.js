8015c1c0724e93f3c8a28fbb1ed1a13d13352489
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-04-29 12:30:18 +0900

    Update ［1차］ 캐시.js

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -7,12 +7,18 @@ function solution(cacheSize, cities) {
         let findCity = cityArr.find((city) => city === current);
 
         if(!findCity){
+            // 기존 데이터베이스에 없을 경우(캐시 미스), 배열에 넣어주기
             cityArr.push(current);
             if(cityArr.length > cacheSize){
+                // 캐시 크기를 넘어갈 경우, 가장 오래된 데이터베이스에 등록된 도시 빼기
                 cityArr.shift();
             }
+            // 실행 시간 5 추가
             answer += 5;
-        } else{
+        } else {
+            // 기존 데이터베이스에 있는 경우(캐시 히트), 
+            // 해당 도시 이름을 배열에서 제거 했다가 최근 사용되었으므로 배열의 마지막에 추가
+            // 실행 시간 1 추가
             cityArr = cityArr.filter((city) => city !== current);
             cityArr.push(current);
             answer ++;
