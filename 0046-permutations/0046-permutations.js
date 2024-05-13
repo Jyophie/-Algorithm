3586562d2b0acceede0de2ff5982a26d850f68bd
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-05-13 13:04:32 +0900

    Time: 99 ms (5.04%), Space: 57.9 MB (5.04%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -6,12 +6,14 @@ function permute(nums) {
     const permutations = [];
     
     const dfs = (picked, unpicked) => {
+        // 선택되지 않은 값이 없을 때, 지금까지 선택된 숫자들을 넣은 permutations 를 리턴
         if(!unpicked.length) return permutations.push(picked);
-        
+   
+        // 아직 선택되지 않은 숫자를 대상으로 루프를 돌림
         unpicked.forEach((num, i) => {
             dfs(
-            [...picked, num],
-            [...unpicked.slice(0, i), ...unpicked.slice(i+1)]
+            [...picked, num], // 선택된 숫자
+            [...unpicked.slice(0, i), ...unpicked.slice(i+1)] // i번째 자리에 있는 숫자만 제외
             );
         })
     }
