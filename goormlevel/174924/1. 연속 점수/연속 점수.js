e912886b8c6b0a7eea41498ee700adced818d6a6
Sohee Ju <133554119+Jyophie@users.noreply.github.com>
2024-05-14 10:37:26 +0900

    Update 연속 점수.js

--- goormlevel/174924/1. 연속 점수/연속 점수.js
+++ goormlevel/174924/1. 연속 점수/연속 점수.js
@@ -14,16 +14,13 @@ const readline = require('readline');
 	
     let maxSum = Math.max(...scores);
     let currentSum = scores[0];
-    let isContinuous = false;
 
     for(let i = 1; i < scores.length; i++){
         if(scores[i-1] + 1 === scores[i]){
             currentSum += scores[i];
-            isContinuous = true;
         } else {
             maxSum = Math.max(maxSum, currentSum);
             currentSum = scores[i];
-            isContinuous = false;
         }
     }
     maxSum = Math.max(maxSum, currentSum); // 마지막 연속된 부분의 합과 비교
